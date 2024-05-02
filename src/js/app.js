7d15607249918dbd855a552559112275c600293e
toshadvidrine <159669056+toshadvidrine@users.noreply.github.com>
2024-05-02 18:42:28 +0000

    socials and right/left

--- src/js/app.js
+++ src/js/app.js
@@ -35,12 +35,12 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name?variables.name:"Lucy"}${variables.lastName?variables.lastName:"Boilett"}</h1>
           <h2>${variables.role?variables.role:"Web Developer,Floor Planner, Technial Writer"}</h2>
-          <h3>${variables.city?variables.city:"Miami,Toronto,Munich,Caracas"}${variables.country?variables.country:"USA,Canada,Germany,Venezuela"}</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h3>${variables.city?variables.city+",":"Miami,Toronto,Munich,Caracas"}${variables.country?variables.country:"USA,Canada,Germany,Venezuela"}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
